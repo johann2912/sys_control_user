b18c23557f8e21b239329b1d3428a4146f7b655e
Johann Gomez <johanngomez02@gmail.com>
2021-06-10 09:45:18 -0500

    estableciendo conexion a la DB

--- index.js
+++ index.js
@@ -10,10 +10,18 @@ app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 // Conexión data base
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.ncdk5.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+const option = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(uri, option)
+.then(() => console.log('Base de datos conectada'))
+.catch(e => console.log('error db:', e))
 
 // import routes
+const authRoutes = require('./routes/auth');
 
 // route middlewares
+app.use('/users', authRoutes);
+
 app.get('/', (req, res) => {
     res.json({
         estado: true,
